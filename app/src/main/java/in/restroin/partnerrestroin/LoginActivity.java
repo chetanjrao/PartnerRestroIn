71b55ca598c76efdff0203ff4914e290949df442
Chethan Jagannatha Kulkarni <chethanjkulkarni@gmail.com>
2018-08-07 19:26:39 +0530

    Updated UI for login. Added Dependency for Retrofit.

--- app/src/main/java/in/restroin/partnerrestroin/LoginActivity.java
+++ app/src/main/java/in/restroin/partnerrestroin/LoginActivity.java
@@ -5,20 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button button = (Button) findViewById(R.id.analytics_button);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent go_to_analytics = new Intent(LoginActivity.this, AnalyticsActivity.class);
-                startActivity(go_to_analytics);
-            }
-        });
     }
 }
