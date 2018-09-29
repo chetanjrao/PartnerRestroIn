452b7a2e1b50074e2e208a792e50e311cc61e259
chethanjkulkarni <chethanjkulkarni@gmail.com>
2018-09-29 14:29:34 +0530

    Using new version of coupons adapter

--- app/src/main/java/in/restroin/partnerrestroin/CouponsActivity.java
+++ app/src/main/java/in/restroin/partnerrestroin/CouponsActivity.java
@@ -1,6 +1,7 @@
 package in.restroin.partnerrestroin;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,8 +12,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.hanks.library.AnimateCheckBox;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,7 +49,12 @@ public class CouponsActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coupons);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.checkboxes_recyclerView);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.checkboxes_recyclerView);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        final TextView textView = (TextView) findViewById(R.id.error_text_view);
+        progressBar.setVisibility(View.VISIBLE);
+        textView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
         showCoupons(recyclerView, selectedCoupons);
@@ -53,20 +62,25 @@ public class CouponsActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.GONE);
                 PartnerRestroINClient client = retrofit.create(PartnerRestroINClient.class);
                 SavedPreferences savedPreferences = new SavedPreferences();
                 Call<MessageModel> call = client.updateCoupons(savedPreferences.getApiKey(CouponsActivity.this), selectedCoupons.toString(), savedPreferences.getRestaurantID(CouponsActivity.this), savedPreferences.getPartnerID(CouponsActivity.this));
                 call.enqueue(new Callback<MessageModel>() {
                     @Override
                     public void onResponse(Call<MessageModel> call, Response<MessageModel> response) {
-                        String message = response.body().getMessage();
-                        Toast.makeText(CouponsActivity.this, "Message: " + message, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(CouponsActivity.this, "Coupons Updated Successfully", Toast.LENGTH_SHORT).show();
                         finish();
                     }
 
                     @Override
                     public void onFailure(Call<MessageModel> call, Throwable t) {
-
+                        progressBar.setVisibility(View.GONE);
+                        textView.setVisibility(View.GONE);
+                        recyclerView.setVisibility(View.VISIBLE);
+                        Toast.makeText(CouponsActivity.this, "Something Went Wrong. Please try again :-( ", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
@@ -110,26 +124,34 @@ public class CouponsActivity extends AppCompatActivity {
         Call<List<CouponsModel>> call = client.getCoupons();
         call.enqueue(new Callback<List<CouponsModel>>() {
             @Override
-            public void onResponse(Call<List<CouponsModel>> call, Response<List<CouponsModel>> response) {
+            public void onResponse(@NonNull final Call<List<CouponsModel>> call,@NonNull Response<List<CouponsModel>> response) {
                 for(int i=0; i < response.body().size(); i++){
-                    coupons.add(new CouponsModel(response.body().get(i).getCoupon_id(), response.body().get(i).getCoupon_code(), response.body().get(i).getDescription()));
+                    coupons.add(new CouponsModel(response.body().get(i).getCoupon_id(), response.body().get(i).getCoupon_code(), response.body().get(i).getDescription(), response.body().get(i).getCoupon_image()));
                 }
                 CouponsAdapter adapter = new CouponsAdapter(coupons);
+                ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+                TextView textView = (TextView) findViewById(R.id.error_text_view);
+                progressBar.setVisibility(View.GONE);
+                textView.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
                 recyclerView.setAdapter(adapter);
                 recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
                     @Override
                     public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
                         final View childView = rv.findChildViewUnder(e.getX(), e.getY());
                         if(childView != null && gestureDetector.onTouchEvent(e)) {
-                            CheckBox checkBox = (CheckBox) childView.findViewById(R.id.coupon_check_box__);
-                            final TextView coupon_id = (TextView) childView.findViewById(R.id.coupon_id__);
-                            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            int position = rv.getChildAdapterPosition(childView);
+                            final AnimateCheckBox checkBox = (AnimateCheckBox) rv.getChildAt(position).findViewById(R.id.coupon_check_box);
+                            final TextView coupon_id = (TextView) rv.getChildAt(position).findViewById(R.id.coupon_id);
+                            checkBox.setOnCheckedChangeListener(new AnimateCheckBox.OnCheckedChangeListener() {
                                 @Override
-                                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                public void onCheckedChanged(View buttonView, boolean isChecked) {
                                     if(isChecked){
                                         selectedCoupons.add('"' + coupon_id.getText().toString() + '"');
+                                        Toast.makeText(CouponsActivity.this, "" + selectedCoupons.toString(), Toast.LENGTH_SHORT).show();
                                     } else {
                                         selectedCoupons.remove('"' + coupon_id.getText().toString() + '"');
+                                        Toast.makeText(CouponsActivity.this, "" + selectedCoupons.toString(), Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
@@ -150,8 +172,12 @@ public class CouponsActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onFailure(Call<List<CouponsModel>> call, Throwable t) {
-
+            public void onFailure(@NonNull Call<List<CouponsModel>> call,@NonNull Throwable t) {
+                ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+                TextView textView = (TextView) findViewById(R.id.error_text_view);
+                progressBar.setVisibility(View.GONE);
+                textView.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
             }
         });
     }
--- app/src/main/java/in/restroin/partnerrestroin/adapters/CouponsAdapter.java
+++ app/src/main/java/in/restroin/partnerrestroin/adapters/CouponsAdapter.java
@@ -1,13 +1,22 @@
 package in.restroin.partnerrestroin.adapters;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.hanks.library.AnimateCheckBox;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,28 +32,56 @@ public class CouponsAdapter extends RecyclerView.Adapter<CouponsAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        CheckBox checkedTextView;
-        TextView description, coupon_id;
+        AnimateCheckBox coupon_checkBox;
+        CardView cardView;
+        TextView coupon_name, description, coupon_id;
+        ImageView  coupon_image;
         public ViewHolder(View itemView) {
             super(itemView);
-            checkedTextView = (CheckBox) itemView.findViewById(R.id.coupon_check_box__);
+            cardView = (CardView) itemView.findViewById(R.id.cardLayoutCoupon);
+            coupon_name = (TextView) itemView.findViewById(R.id.coupon_name);
+            coupon_checkBox = (AnimateCheckBox) itemView.findViewById(R.id.coupon_check_box);
             description = (TextView) itemView.findViewById(R.id.coupon_description);
-            coupon_id = (TextView) itemView.findViewById(R.id.coupon_id__);
+            coupon_id = (TextView) itemView.findViewById(R.id.coupon_id);
+            coupon_image = (ImageView) itemView.findViewById(R.id.coupon_image);
         }
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_check_box_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_coupon_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
-        holder.checkedTextView.setText(couponsModels.get(position).getCoupon_code());
-        holder.description.setText("Description: " + couponsModels.get(position).getDescription());
+        holder.coupon_checkBox.setChecked(false);
+        holder.coupon_name.setText(couponsModels.get(position).getCoupon_code());
+        holder.description.setText(couponsModels.get(position).getDescription());
         holder.coupon_id.setText(couponsModels.get(position).getCoupon_id());
+        Uri coupon_image_path = Uri.parse("https://www.restroin.in/" + couponsModels.get(position).getCoupon_image());
+        Picasso.get().load(coupon_image_path).into(holder.coupon_image, new Callback() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (holder.coupon_checkBox.isChecked()){
+                    holder.coupon_checkBox.setChecked(false);
+                } else {
+                    holder.coupon_checkBox.setChecked(false);
+                }
+            }
+        });
     }
 
     @Override
